7a7b638c511378609c679e73d55e59961afb273e
marie8bit <yd7581ku@go.minneapolis.edu>
2017-02-16 14:19:22 -0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup (name = 'AsciiArt',
+    version = '0.1.01',
+    description = 'Ascii "art" from images',
+    author = 'clara, marie',
+    packages = ['asciiArt'],
+    install_requires = ['pillow', 'requests'])
